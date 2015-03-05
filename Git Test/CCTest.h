8b0bf5d4801ee4c2c89096f140e2eb6e7385cd00
Maddock <maddock1010@gmail.com>
2015-03-05 15:46:30 +0800

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Maddock on 2015/3/5.
+//  Copyright (c) 2015年 Maddock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Maddock on 2015/3/5.
+//  Copyright (c) 2015年 Maddock. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
